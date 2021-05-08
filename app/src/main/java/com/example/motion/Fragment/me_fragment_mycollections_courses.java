e8b6221b46bb6504ec2fe57d03d1075b26ccfb26
Stanly19 <1446732363@qq.com>
2021-05-08 16:59:04 +0800

    1

--- app/src/main/java/com/example/motion/Fragment/me_fragment_mycollections_courses.java
+++ app/src/main/java/com/example/motion/Fragment/me_fragment_mycollections_courses.java
@@ -1,9 +1,11 @@
 package com.example.motion.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,45 +13,142 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chad.library.adapter.base.listener.OnLoadMoreListener;
 import com.example.motion.Entity.MultipleItem;
 import com.example.motion.Entity.me_mycourse_collections;
 import com.example.motion.R;
+import com.example.motion.Utils.HttpUtils;
 import com.example.motion.Widget.MultipleItemQuickAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class me_fragment_mycollections_courses extends Fragment {
     private List<List> dataSet = new ArrayList();
     private List<MultipleItem> collectionsList = new ArrayList();
-
+    private int TOTAL_PAGES;
     MultipleItemQuickAdapter quickAdapter;
     RecyclerView recyclerView;
 
+    private int httpcode;
+    private int currentPage; //要分页查询的页面
+    private Boolean hasNext;
+    private String token = "123";
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        currentPage = 1;
         View view = inflater.inflate(R.layout.me_fragment_mycourse_collections,container,false);
-        initData();
+        //initData();
         initView(view);
+        //configLoadMoreData();
         return view;
     }
 
     public void initView(View view){
         recyclerView = (RecyclerView) view.findViewById(R.id.meMyCollectionsRecyclerView);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         quickAdapter = new MultipleItemQuickAdapter(collectionsList);
+
+        configLoadMoreData();
+
+        quickAdapter.getLoadMoreModule().setOnLoadMoreListener(new OnLoadMoreListener() {
+            //int mCurrentCunter = 0;
+            @Override
+            public void onLoadMore() {
+                if (currentPage > TOTAL_PAGES) {
+                    quickAdapter.getLoadMoreModule().loadMoreEnd();
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            configLoadMoreData();
+                        }
+                    }, 1000);
+
+                }
+
+            }
+        });
+
         recyclerView.setAdapter(quickAdapter);
     }
-    public void initData(){
+//    public void initData(){
+//
+//        me_mycourse_collections collections;
+//        for (int i = 0; i < 5; i++) {
+//            collections = new me_mycourse_collections();
+//            collectionsList.add(new MultipleItem(12,collections));
+//            collections.setImgUrls("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3385472845,2539383542&fm=11&gp=0.jpg");
+//            collections.setCollectionsName1("写代码");
+//            collections.setCollectionsName2("数据结构");
+//            collections.setCollectionsName3("学习");
+//        }
+//        dataSet.add(collectionsList);
+//    }
 
-        me_mycourse_collections collections;
-        for (int i = 0; i < 5; i++) {
-            collections = new me_mycourse_collections();
-            collectionsList.add(new MultipleItem(12,collections));
-            collections.setImgUrls("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3385472845,2539383542&fm=11&gp=0.jpg");
-            collections.setCollectionsName1("写代码");
-            collections.setCollectionsName2("数据结构");
-            collections.setCollectionsName3("学习");
+    private void getHttpSearch(final String url) {
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String responseData = null;
+                try {
+                    responseData = HttpUtils.connectHttpGet(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                JSONObject jsonObject1 = null;
+                try {
+                    jsonObject1 = new JSONObject(responseData);
+                    httpcode = jsonObject1.getInt("code");
+                    if (httpcode == 200) {
+                        JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
+                        hasNext = jsonObject2.getBoolean("hasNext");
+                        TOTAL_PAGES = jsonObject2.getInt("pageSize");
+                        //得到筛选的课程list
+                        JSONArray JSONArrayCollections = jsonObject2.getJSONArray("collectionsList");
+                        for (int i = 0; i < JSONArrayCollections.length(); i++) {
+                            JSONObject jsonObject = JSONArrayCollections.getJSONObject(i);
+                            //相应的内容
+                            me_mycourse_collections collections = new me_mycourse_collections();
+                            collections.setCollectionsName1(jsonObject.getString("courseName"));
+                            collections.setCollectionsName2(jsonObject.getString("targetAge"));
+                            collections.setCollectionsName3(jsonObject.getString("labels"));
+                            collectionsList.add(new MultipleItem(12,collections));
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        if (httpcode != 200) {
+            Toast.makeText(getActivity(), "ERROR", Toast.LENGTH_SHORT).show();
+        }
+/*        else for (int i = 0; i <courseList.size(); i++)btCourse[i].setText(courseList.get(i).getCourseName() + "\n" + courseList.get(i).getDegree() + " . " +
+                courseList.get(i).getDuration() + " . " +courseList.get(i).getHits() + "万人已参加");//展示课程*/
+    }
+
+    private void configLoadMoreData() {
+        String url;//http请求的url
+        url = "http://10.34.25.45:8080/api/user/userHasCollected?size="+1+"&page="+1+"&token="+token;
+        getHttpSearch(url);
         dataSet.add(collectionsList);
+        quickAdapter.addData(dataSet.get(currentPage-1));
+        currentPage++;
+        quickAdapter.getLoadMoreModule().loadMoreEnd();
     }
 }
--- app/src/main/java/com/example/motion/Fragment/sport_fragment_main.java
+++ app/src/main/java/com/example/motion/Fragment/sport_fragment_main.java
@@ -1,28 +1,148 @@
 package com.example.motion.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.motion.R;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarLayout;
+import com.haibin.calendarview.CalendarView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class sport_fragment_main extends Fragment {
+public class sport_fragment_main extends Fragment  implements
+        CalendarView.OnCalendarSelectListener,
+        CalendarView.OnYearChangeListener,
+        View.OnClickListener{
+    TextView mTextMonthDay;
+    TextView mTextYear;
+    TextView mTextLunar;
+    TextView mTextCurrentDay;
+    CalendarView mCalendarView;
+    RelativeLayout mRelativeTool;
+    private int mYear;
+    CalendarLayout mCalendarLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sport_fragment_main, container, false);
         initView(view);
+        initData();
         return view;
     }
 
     private void initView(View view){
         ImageView img2= view.findViewById(R.id.diet_main_search);
+        //setStatusBarDarkMode();
+        mTextMonthDay = view.findViewById(R.id.tv_month_day);
+        mTextYear = view.findViewById(R.id.tv_year);
+        mTextLunar = view.findViewById(R.id.tv_lunar);
+        mRelativeTool = view.findViewById(R.id.rl_tool);
+        mCalendarView = view.findViewById(R.id.calendarView);
+        mTextCurrentDay =  view.findViewById(R.id.tv_current_day);
+        mTextMonthDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mCalendarLayout.isExpand()) {
+                    mCalendarLayout.expand();
+                    return;
+                }
+                mCalendarView.showYearSelectLayout(mYear);
+                mTextLunar.setVisibility(View.GONE);
+                mTextYear.setVisibility(View.GONE);
+                mTextMonthDay.setText(String.valueOf(mYear));
+            }
+        });
+        view.findViewById(R.id.fl_current).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarView.scrollToCurrent();
+            }
+        });
+        mCalendarLayout = view.findViewById(R.id.calendarLayout);
+        mCalendarView.setOnCalendarSelectListener(this);
+        mCalendarView.setOnYearChangeListener(this);
+        mTextYear.setText(String.valueOf(mCalendarView.getCurYear()));
+        mYear = mCalendarView.getCurYear();
+        mTextMonthDay.setText(mCalendarView.getCurMonth() + "月" + mCalendarView.getCurDay() + "日");
+        mTextLunar.setText("今日");
+        mTextCurrentDay.setText(String.valueOf(mCalendarView.getCurDay()));
+    }
+    protected int getLayoutId() {
+        return R.id.colorfulCalendar;
+    }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+
+    }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+
+    }
+
+    @Override
+    public void onYearChange(int year) {
+
+    }
+
+
+    protected void initData() {
+
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+        Map<String, Calendar> map = new HashMap<>();
+        int[] countDay;
+        Log.d("initData","year,motnth"+year+"/"+month);
+        countDay = new int[]{ 1,2,3,5,6,7,8,9,11,12,15,16,17,18,19,21,24,25,27,29,30,0,0,0,0,0,0,0,0,0,0};
+        for(int i = 0; i < countDay.length;i++) {//设置变色的日子
+            map.put(getSchemeCalendar(year, month, countDay[i], 0xFFbc13f0, "假").toString(),
+                    getSchemeCalendar(year, month, countDay[i], 0xFFbc13f0, "假"));
+            /*map.put(getSchemeCalendar(year, month, 3, 0xFFe69138, "事").toString(),
+                    getSchemeCalendar(year, month, 3, 0xFFe69138, "事"));
+            map.put(getSchemeCalendar(year, month, 9, 0xFFdf1356, "议").toString(),
+                    getSchemeCalendar(year, month, 9, 0xFFdf1356, "议"));
+            map.put(getSchemeCalendar(year, month, 13, 0xFFedc56d, "记").toString(),
+                    getSchemeCalendar(year, month, 13, 0xFFedc56d, "记"));
+            map.put(getSchemeCalendar(year, month, 14, 0xFFedc56d, "记").toString(),
+                    getSchemeCalendar(year, month, 14, 0xFFedc56d, "记"));
+            map.put(getSchemeCalendar(year, month, 15, 0xFFaacc44, "假").toString(),
+                    getSchemeCalendar(year, month, 15, 0xFFaacc44, "假"));
+            map.put(getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记").toString(),
+                    getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记"));
+            map.put(getSchemeCalendar(year, month, 25, 0xFF13acf0, "假").toString(),
+                    getSchemeCalendar(year, month, 25, 0xFF13acf0, "假"));
+            map.put(getSchemeCalendar(year, month, 27, 0xFF13acf0, "多").toString(),
+                    getSchemeCalendar(year, month, 27, 0xFF13acf0, "多"));*/
+            //此方法在巨大的数据量上不影响遍历性能，推荐使用
+            mCalendarView.setSchemeDate(map);
+        }
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        return calendar;
     }
 }
